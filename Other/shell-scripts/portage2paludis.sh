d377cd3293fd399494491a1496fa0908a0d001ff
john <john@jmd1.(none)>
2008-10-12 00:10:10 -0400

    Updated codeblocks added portage2paludis script

---
+++ Other/shell-scripts/portage2paludis.sh
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<!-- git web interface version 1.5.3.7, (C) 2005-2006, Kay Sievers <kay.sievers@vrfy.org>, Christian Gierke -->
+<!-- git core binaries version 1.5.6.4 -->
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="generator" content="gitweb/1.5.3.7 git/1.5.6.4"/>
+<meta name="robots" content="index, nofollow"/>
+<title>git.pioto.org Git - paludis-scripts.git/summary</title>
+<link rel="stylesheet" type="text/css" href="/gitweb.css"/>
+<link rel="alternate" title="paludis-scripts.git log RSS feed" href="/?p=paludis-scripts.git;a=rss" type="application/rss+xml" />
+<link rel="alternate" title="paludis-scripts.git log RSS feed (no merges)" href="/?p=paludis-scripts.git;a=rss;opt=--no-merges" type="application/rss+xml" />
+<link rel="alternate" title="paludis-scripts.git log Atom feed" href="/?p=paludis-scripts.git;a=atom" type="application/atom+xml" />
+<link rel="alternate" title="paludis-scripts.git log Atom feed (no merges)" href="/?p=paludis-scripts.git;a=atom;opt=--no-merges" type="application/atom+xml" />
+<link rel="shortcut icon" href="/git-favicon.png" type="image/png"/>
+</head>
+<body>
+<div class="page_header">
+<a title="git homepage" href="http://git.or.cz/"><img src="/git-logo.png" width="72" height="27" alt="git" class="logo"/></a><a href="/">projects</a> / <a href="/?p=paludis-scripts.git;a=summary">paludis-scripts.git</a> / summary
+</div>
+<form method="get" action="/" enctype="application/x-www-form-urlencoded">
+<div class="search">
+<input type="hidden" name="p" value="paludis-scripts.git"  />
+<input type="hidden" name="a" value="search"  />
+<input type="hidden" name="h" value="HEAD"  />
+<select name="st" >
+<option selected="selected" value="commit">commit</option>
+<option value="grep">grep</option>
+<option value="author">author</option>
+<option value="committer">committer</option>
+<option value="pickaxe">pickaxe</option>
+</select><sup><a href="/?p=paludis-scripts.git;a=search_help">?</a></sup> search:
+<input type="text" name="s"  />
+</div>
+</form>
+<div class="page_nav">
+summary | <a href="/?p=paludis-scripts.git;a=shortlog">shortlog</a> | <a href="/?p=paludis-scripts.git;a=log">log</a> | <a href="/?p=paludis-scripts.git;a=commit;h=2f416b27629f71d3a256073a18f10320d1976d45">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=2f416b27629f71d3a256073a18f10320d1976d45">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree">tree</a><br/>
+<br/>
+</div>
+<div class="title">&nbsp;</div>
+<table cellspacing="0">
+<tr><td>description</td><td>Miscellaneous scripts related to Paludis.</td></tr>
+<tr><td>owner</td><td>ciaranm</td></tr>
+<tr><td>last change</td><td>Mon, 29 Sep 2008 23:22:21 +0000</td></tr>
+<tr><td>URL</td><td>git://git.pioto.org/paludis-scripts.git</td></tr>
+<tr><td></td><td>git+ssh://git@git.pioto.org/paludis-scripts.git</td></tr>
+</table>
+<div class="header">
+<a class="title" href="/?p=paludis-scripts.git;a=shortlog">shortlog</a>
+</div>
+<table class="shortlog" cellspacing="0">
+<tr class="dark">
+<td title="2008-09-29"><i>12 days ago</i></td>
+<td><i>David Shakaryan</i></td>
+<td><a class="list subject" title="pint.rb: Do not create a new environment for each dependency spec." href="/?p=paludis-scripts.git;a=commit;h=2f416b27629f71d3a256073a18f10320d1976d45">pint.rb: Do not create a new environment for each depen ... <span class="refs"> <span class="head" title="heads/master">master</span></span></a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=2f416b27629f71d3a256073a18f10320d1976d45">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=2f416b27629f71d3a256073a18f10320d1976d45">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=2f416b27629f71d3a256073a18f10320d1976d45;hb=2f416b27629f71d3a256073a18f10320d1976d45">tree</a></td>
+</tr>
+<tr class="light">
+<td title="3 weeks ago"><i>2008-09-15</i></td>
+<td><i>Mike Kelly</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=f54c03754ef83870b43416dc00d4ecba90c49675">De-gentoo-ify my address.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=f54c03754ef83870b43416dc00d4ecba90c49675">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=f54c03754ef83870b43416dc00d4ecba90c49675">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=f54c03754ef83870b43416dc00d4ecba90c49675;hb=f54c03754ef83870b43416dc00d4ecba90c49675">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="3 weeks ago"><i>2008-09-15</i></td>
+<td><i>Mike Kelly</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=968685d5b586f56f8c148581b505dde84160e921">Fix a bug with splitting comments in portage2paludis.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=968685d5b586f56f8c148581b505dde84160e921">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=968685d5b586f56f8c148581b505dde84160e921">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=968685d5b586f56f8c148581b505dde84160e921;hb=968685d5b586f56f8c148581b505dde84160e921">tree</a></td>
+</tr>
+<tr class="light">
+<td title="4 weeks ago"><i>2008-09-11</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=53057e271f2e804f8bca78dda0fa9f57989acfac">Use new Filter::Slot</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=53057e271f2e804f8bca78dda0fa9f57989acfac">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=53057e271f2e804f8bca78dda0fa9f57989acfac">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=53057e271f2e804f8bca78dda0fa9f57989acfac;hb=53057e271f2e804f8bca78dda0fa9f57989acfac">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="4 weeks ago"><i>2008-09-08</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=3de2d0f1b9fe8a886cd13fb8d625fd954bf1f065">Remove cruft.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=3de2d0f1b9fe8a886cd13fb8d625fd954bf1f065">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=3de2d0f1b9fe8a886cd13fb8d625fd954bf1f065">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=3de2d0f1b9fe8a886cd13fb8d625fd954bf1f065;hb=3de2d0f1b9fe8a886cd13fb8d625fd954bf1f065">tree</a></td>
+</tr>
+<tr class="light">
+<td title="5 weeks ago"><i>2008-09-04</i></td>
+<td><i>Mike Kelly</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=b624d1b80039ca70cd12e99b4403f327daf50aba">Generate a layman unavailable repo.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=b624d1b80039ca70cd12e99b4403f327daf50aba">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=b624d1b80039ca70cd12e99b4403f327daf50aba">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=b624d1b80039ca70cd12e99b4403f327daf50aba;hb=b624d1b80039ca70cd12e99b4403f327daf50aba">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="7 weeks ago"><i>2008-08-17</i></td>
+<td><i>Ingmar Vanhassel</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=fc4cd764ad7bbae0d1f871c39cb72856d0e29e44">Import omp's pint.rb.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=fc4cd764ad7bbae0d1f871c39cb72856d0e29e44">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=fc4cd764ad7bbae0d1f871c39cb72856d0e29e44">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=fc4cd764ad7bbae0d1f871c39cb72856d0e29e44;hb=fc4cd764ad7bbae0d1f871c39cb72856d0e29e44">tree</a></td>
+</tr>
+<tr class="light">
+<td title="7 weeks ago"><i>2008-08-17</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=c7fa5482897753235340f3606249fde65692384f">Update scripts for current Paludis master.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=c7fa5482897753235340f3606249fde65692384f">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=c7fa5482897753235340f3606249fde65692384f">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=c7fa5482897753235340f3606249fde65692384f;hb=c7fa5482897753235340f3606249fde65692384f">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="2 months ago"><i>2008-07-23</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=4e55f58f318f307bf5a9e419cc15dafab44c0345">Fix config-decruft for API changes.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=4e55f58f318f307bf5a9e419cc15dafab44c0345">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=4e55f58f318f307bf5a9e419cc15dafab44c0345">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=4e55f58f318f307bf5a9e419cc15dafab44c0345;hb=4e55f58f318f307bf5a9e419cc15dafab44c0345">tree</a></td>
+</tr>
+<tr class="light">
+<td title="2 months ago"><i>2008-07-23</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=65883b0a149e4c42e1863c269e4266a44fa1b4e8">Kill 0.26 config-decruft and depgraph2svg.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=65883b0a149e4c42e1863c269e4266a44fa1b4e8">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=65883b0a149e4c42e1863c269e4266a44fa1b4e8">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=65883b0a149e4c42e1863c269e4266a44fa1b4e8;hb=65883b0a149e4c42e1863c269e4266a44fa1b4e8">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="3 months ago"><i>2008-07-02</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" title="Fix gimme.rb for new Selection and parse_user_package_dep_spec." href="/?p=paludis-scripts.git;a=commit;h=e6a1bd0073b6d6213cd5f5a451ab29ccb0f8278a">Fix gimme.rb for new Selection and parse_user_package ...</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=e6a1bd0073b6d6213cd5f5a451ab29ccb0f8278a">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=e6a1bd0073b6d6213cd5f5a451ab29ccb0f8278a">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=e6a1bd0073b6d6213cd5f5a451ab29ccb0f8278a;hb=e6a1bd0073b6d6213cd5f5a451ab29ccb0f8278a">tree</a></td>
+</tr>
+<tr class="light">
+<td title="3 months ago"><i>2008-06-15</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=c652da89716efb351bf3167cf257ead380c2032b">Update to the new parse_user_package_dep_spec.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=c652da89716efb351bf3167cf257ead380c2032b">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=c652da89716efb351bf3167cf257ead380c2032b">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=c652da89716efb351bf3167cf257ead380c2032b;hb=c652da89716efb351bf3167cf257ead380c2032b">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="4 months ago"><i>2008-06-09</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" title="Update config-decruft and depgraph2svg for current trunk." href="/?p=paludis-scripts.git;a=commit;h=77498b6da432a2a171797b1bacd55e7a19bf6956">Update config-decruft and depgraph2svg for current ...</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=77498b6da432a2a171797b1bacd55e7a19bf6956">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=77498b6da432a2a171797b1bacd55e7a19bf6956">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=77498b6da432a2a171797b1bacd55e7a19bf6956;hb=77498b6da432a2a171797b1bacd55e7a19bf6956">tree</a></td>
+</tr>
+<tr class="light">
+<td title="4 months ago"><i>2008-06-01</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=00d7bf771ed161b8caa7664aaee9266746a20e64">Remove old cruft.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=00d7bf771ed161b8caa7664aaee9266746a20e64">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=00d7bf771ed161b8caa7664aaee9266746a20e64">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=00d7bf771ed161b8caa7664aaee9266746a20e64;hb=00d7bf771ed161b8caa7664aaee9266746a20e64">tree</a></td>
+</tr>
+<tr class="dark">
+<td title="4 months ago"><i>2008-06-01</i></td>
+<td><i>David Leverton</i></td>
+<td><a class="list subject" href="/?p=paludis-scripts.git;a=commit;h=1917c27f763e5dd01f988e73a8358d7b3c517479">Typo.</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=1917c27f763e5dd01f988e73a8358d7b3c517479">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=1917c27f763e5dd01f988e73a8358d7b3c517479">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=1917c27f763e5dd01f988e73a8358d7b3c517479;hb=1917c27f763e5dd01f988e73a8358d7b3c517479">tree</a></td>
+</tr>
+<tr class="light">
+<td title="5 months ago"><i>2008-05-02</i></td>
+<td><i>Mike Kelly</i></td>
+<td><a class="list subject" title="Fix handling of directory syntax for keywords and use flags (from mmgr)." href="/?p=paludis-scripts.git;a=commit;h=09e8b1c25db8ad88c5f936d8355105302e20a75d">Fix handling of directory syntax for keywords and use ...</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=commit;h=09e8b1c25db8ad88c5f936d8355105302e20a75d">commit</a> | <a href="/?p=paludis-scripts.git;a=commitdiff;h=09e8b1c25db8ad88c5f936d8355105302e20a75d">commitdiff</a> | <a href="/?p=paludis-scripts.git;a=tree;h=09e8b1c25db8ad88c5f936d8355105302e20a75d;hb=09e8b1c25db8ad88c5f936d8355105302e20a75d">tree</a></td>
+</tr>
+<tr>
+<td colspan="4"><a href="/?p=paludis-scripts.git;a=shortlog">...</a></td>
+</tr>
+</table>
+<div class="header">
+<a class="title" href="/?p=paludis-scripts.git;a=heads">heads</a>
+</div>
+<table class="heads" cellspacing="0">
+<tr class="dark">
+<td><i>12 days ago</i></td>
+<td class="current_head"><a class="list name" href="/?p=paludis-scripts.git;a=shortlog;h=master">master</a></td>
+<td class="link"><a href="/?p=paludis-scripts.git;a=shortlog;h=master">shortlog</a> | <a href="/?p=paludis-scripts.git;a=log;h=master">log</a> | <a href="/?p=paludis-scripts.git;a=tree;h=master;hb=master">tree</a></td>
+</tr></table>
+<div class="page_footer">
+<div class="page_footer_text">Miscellaneous scripts related to Paludis.</div>
+<a class="rss_logo" href="/?p=paludis-scripts.git;a=rss">RSS</a> <a class="rss_logo" href="/?p=paludis-scripts.git;a=atom">Atom</a>
+</div>
+</body>
+</html>

acc907441245c8469ee693a33f35ae9b53a19682
John M. Drescher <drescherjm@gmail.com>
2008-10-29 00:33:54 -0400

    Updated keywords

--- Other/shell-scripts/sortkw.sh
+++ Other/shell-scripts/sortkw.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 if [ -e $1 ]; then
-
-sort $1 | sed -e 's/[ \t]*$//' -e s/~amd64//g -e s/~x86//g | uniq  > /tmp/keywords.sorted
-cat /tmp/keywords.sorted > $1
+TMPFILE=`mktemp /tmp/keywords.sorted.XXXXXXXXXX`
+sort $1 | sed -e 's/[ \t]*$//' -e s/~amd64//g -e s/~x86//g | uniq  > ${TMPFILE}
+cat ${TMPFILE} > $1
+rm ${TMPFILE}
 
 fi

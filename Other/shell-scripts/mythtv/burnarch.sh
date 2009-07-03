6af331cfc228ae691669e79dff61310669c0aa2d
John M. Drescher <drescherjm@gmail.com>
2009-07-03 19:06:16 -0400

    Bump mythtv vdpau to svn rev 20744 with latest JYA patch

--- Other/shell-scripts/mythtv/burnarch.sh
+++ Other/shell-scripts/mythtv/burnarch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ulimit -l unlimited
+umount /dev/dvd -fl
 /usr/bin/growisofs -Z /dev/dvd=/var/tmp/mythtv/work/mythburn.iso -use-the-force-luke=notray -use-the-force-luke=tty -dvd-compat -speed=4
 eject /dev/dvd
 

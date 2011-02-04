7e7fcffb05b85605f2794111946e89c6e7820e69
John M. Drescher <drescherjm@gmail.com>
2011-02-04 10:55:14 -0500

    added a script to print the health status and serial number of every drive in md raid arrays

---
+++ Other/shell-scripts/examine_mdraid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function process_device()
+{
+        echo -n ${device}
+        hdparm -I /dev/${device} | grep "al Number"
+        smartctl --all /dev/${device} | grep -e "Reallocated_Sector_Ct" -e "Current_Pending_Sector" -e "Offline_Uncorrectable" -e "UDMA_CRC_Error_Count" -e "Hardware_ECC_Recovered"
+        echo
+}
+
+for a in /dev/sd?;
+do
+        device=${a/\/dev\//}
+        grep ${device} /proc/mdstat >> /dev/null
+        if [ $? == 0 ];
+        then
+                process_device
+        fi
+done
+

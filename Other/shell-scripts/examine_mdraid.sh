409c2db52be894b54d74bcf952c185474095eb0b
John Drescher <drescherjm@gmail.com>
2011-10-29 10:14:09 -0400

    Updated examine_mdraid.sh

--- Other/shell-scripts/examine_mdraid.sh
+++ Other/shell-scripts/examine_mdraid.sh
@@ -4,7 +4,7 @@ function process_device()
 {
         echo -n ${device}
         hdparm -I /dev/${device} | grep "al Number"
-        smartctl --all /dev/${device} | grep -e "Reallocated_Sector_Ct" -e "Current_Pending_Sector" -e "Offline_Uncorrectable" -e "UDMA_CRC_Error_Count" -e "Hardware_ECC_Recovered"
+        smartctl --all /dev/${device} | grep -e "Reallocated_Sector_Ct" -e "Current_Pending_Sector" -e "Offline_Uncorrectable" -e "UDMA_CRC_Error_Count" -e "Hardware_ECC_Recovered" -e "Power_On_Hours"
         echo
 }
 

62e05ec7a353657e40ed607ebc20d6e4104c9017
John M. Drescher <drescherjm@gmail.com>
2012-03-14 22:19:40 -0400

    Updated genkernel scripts.

--- Other/shell-scripts/fix_genkernel.sh
+++ Other/shell-scripts/fix_genkernel.sh
@@ -7,5 +7,5 @@ sed -i s'/# LVM="no"/LVM="yes"/g' /etc/genkernel.conf
 sed -i s'/# MDADM="no"/MDADM="yes"/g' /etc/genkernel.conf 
 sed -i s'/# BOOTLOADER="grub"/BOOTLOADER="grub"/g' /etc/genkernel.conf 
 sed -i s'/# CLEAR_CACHE_DIR="yes"/CLEAR_CACHE_DIR="no"/g' /etc/genkernel.conf 
-sed -i s'-# KERNEL_CC="gcc"-KERNEL_CC="/usr/bin/icecc"-g' /etc/genkernel.conf 
+#sed -i s'-# KERNEL_CC="gcc"-KERNEL_CC="/usr/bin/icecc"-g' /etc/genkernel.conf 
 
--- Other/shell-scripts/gkern.sh
+++ Other/shell-scripts/gkern.sh
@@ -5,7 +5,7 @@ if [ ! -f /usr/src/linux/.config ] ; then
 	zcat /proc/config.gz > /usr/src/linux/.config
 fi
 
-cp /usr/src/linux/.config /usr/share/genkernel/x86_64/kernel-config-2.6
+#cp /usr/src/linux/.config /usr/share/genkernel/x86_64/kernel-config-2.6
 #genkernel $@ --menuconfig all --splash=livecd-2007.0 --save-config --color --install --splash-res=1024x768
 
 genkernel $@ --menuconfig all --save-config --color --install

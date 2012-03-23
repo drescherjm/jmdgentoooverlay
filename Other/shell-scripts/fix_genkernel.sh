040b13557efb2053431f335b8cb4fc3f3fdfe2b5
John M. Drescher <drescherjm@gmail.com>
2012-03-22 20:20:49 -0400

    updated fix_genkernel.sh

--- Other/shell-scripts/fix_genkernel.sh
+++ Other/shell-scripts/fix_genkernel.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sed -i s'#CLEAN="yes"#CLEAN="no"#g' /etc/genkernel.conf 
-sed -i s'#MRPROPER="yes"#MRPROPER="no"#g' /etc/genkernel.conf 
-sed -i s'/# MAKEOPTS="-j2"/MAKEOPTS="-j10"/g' /etc/genkernel.conf 
-sed -i s'/# LVM="no"/LVM="yes"/g' /etc/genkernel.conf 
-sed -i s'/# MDADM="no"/MDADM="yes"/g' /etc/genkernel.conf 
-sed -i s'/# BOOTLOADER="grub"/BOOTLOADER="grub"/g' /etc/genkernel.conf 
-sed -i s'/# CLEAR_CACHE_DIR="yes"/CLEAR_CACHE_DIR="no"/g' /etc/genkernel.conf 
-#sed -i s'-# KERNEL_CC="gcc"-KERNEL_CC="/usr/bin/icecc"-g' /etc/genkernel.conf 
+sed -i s'#[ ]*CLEAN="yes"#CLEAN="no"#g' /etc/genkernel.conf 
+sed -i s'#[ ]*MRPROPER="yes"#MRPROPER="no"#g' /etc/genkernel.conf 
+sed -i s'/#[ ]*MAKEOPTS="-j2"/MAKEOPTS="-j10"/g' /etc/genkernel.conf 
+sed -i s'/#[ ]*LVM="no"/LVM="yes"/g' /etc/genkernel.conf 
+sed -i s'/#[ ]*MDADM="no"/MDADM="yes"/g' /etc/genkernel.conf 
+sed -i s'/#[ ]*BOOTLOADER="grub"/BOOTLOADER="grub"/g' /etc/genkernel.conf 
+sed -i s'/#[ ]*CLEAR_CACHE_DIR="yes"/CLEAR_CACHE_DIR="no"/g' /etc/genkernel.conf 
+#sed -i s'-#[ ]*KERNEL_CC="gcc"-KERNEL_CC="/usr/bin/icecc"-g' /etc/genkernel.conf 
 

3edf00b111211fe1ff36b00374abce63eb1c6365
John M. Drescher <drescherjm@gmail.com>
2012-10-02 12:27:16 -0400

    added update_distfiles

---
+++ Other/shell-scripts/update_distfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sed -i '/distfiles/d' /etc/autofs/auto.auto
+
+echo "distfiles       -fstype=nfs,nfsvers=3,rw,timeo=100,tcp,soft,intr                                        datastore2:/auto/btrfs_data/distfiles" >> /etc/autofs/auto.auto
+
+umount /auto/distfiles -fl
+/etc/init.d/autofs reload
+
+sed -i '/DISTDIR/d' /etc/make.conf
+echo "DISTDIR=\"/auto/distfiles\"" >> /etc/make.conf

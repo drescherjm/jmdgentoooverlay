7ef6bc59243516488c47e3699549ce550cfc4500
John M. Drescher <drescherjm@gmail.com>
2013-07-23 16:33:45 -0400

    added my update script for the hddtemp db

---
+++ Other/shell-scripts/update_hddtemp_db_jmd.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+update_db() {
+	local src=$1
+	local dst=$2
+
+	while read line ; do
+		if [[ -z $(echo "${line}" | sed -re 's/(^#.*|^\w*$)//') ]]; then
+			echo "${line}" >> "${dst}"
+		fi
+
+		id=$(echo "${line}" | grep -o '"[^"]*"')
+
+		grep "${id}" "${dst}" 2>&1 >/dev/null || echo "${line}" >> "${dst}"
+	done < "${src}"
+}
+
+die() {
+	echo "$*"
+	exit 1
+}
+
+echo_red() {	
+	echo -e "\033[1;31m$*\033[0m"
+}
+
+
+
+batch_dir_name="$( cd -P "$( dirname "$0" )" && pwd )"
+
+update_data_file=${batch_dir_name}/data/hddtemp.jmd.db
+
+if [ -e "${update_data_file}" ]; then
+
+  cd /usr/share/hddtemp
+  update_db "${update_data_file}" "hddtemp.db"
+
+else
+  echo_red "Update FAILED. Could not find update file!"
+fi

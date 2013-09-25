64d44665756803c331876c90a1c20db005941a29
John M. Drescher <drescherjm@gmail.com>
2013-09-24 21:03:05 -0400

    Updates

--- Other/shell-scripts/display_drive_temp_warning_stats.sh
+++ Other/shell-scripts/display_drive_temp_warning_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "WARNINGS:"
-grep "TEMPMON: WARNING" /var/log/messages | grep -v "The temperature" | awk '{ print $7,$8, $9 }' | sort | uniq -c
+grep "TEMPMON: WARNING" /var/log/messages | grep -v "temperature" | awk '{ print $7,$8, $9 }' | sort | uniq -c
 
 echo "CRITICAL:"
 grep "TEMPMON: CRITICAL" /var/log/messages | grep -v "There are" | awk '{ print $7,$8, $9 }' | sort | uniq -c

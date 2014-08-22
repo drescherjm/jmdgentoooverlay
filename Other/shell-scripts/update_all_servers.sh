fe8e36070ede28b8a4c46fe54dc1e0642518d915
John M. Drescher <drescherjm@gmail.com>
2014-08-22 12:27:08 -0400

    ip address change for vm-gentoo

--- Other/shell-scripts/update_all_servers.sh
+++ Other/shell-scripts/update_all_servers.sh
@@ -8,10 +8,12 @@ emerge -eF world 2>&1 >> /var/log/update_distfiles.txt &
 
 sh /root/shell-scripts/run_on_all_no_pass.sh emerge --sync
 
-sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.184:/layman/  /var/lib/layman/
+sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.83:/layman/  /var/lib/layman/
 
-sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.184/gentoo-keywords/ /usr/local/gentoo-keywords/
+sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.83/gentoo-keywords/ /usr/local/gentoo-keywords/
 
 sh run_on_all_no_pass.sh emerge -uDvNFp world >> /root/fetch.txt
+sh run_on_all_no_pass.sh emerge -uDvNFp system >> /root/fetch.txt
+sh run_on_all_no_pass.sh emerge -ep world >> /root/fetch.txt
 
 sh /root/shell-scripts/run_on_all_no_pass.sh /root/shell-scripts/glsa-fix.sh

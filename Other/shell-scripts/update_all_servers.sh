2d211ee62a9457916c73061ab6a93475e23a6807
John M. Drescher <drescherjm@gmail.com>
2015-02-16 12:58:12 -0500

    add support for clamav

--- Other/shell-scripts/update_all_servers.sh
+++ Other/shell-scripts/update_all_servers.sh
@@ -6,6 +6,8 @@ eix-sync
 
 emerge -eF world 2>&1 >> /var/log/update_distfiles.txt &
 
+freshclam &
+
 sh /root/shell-scripts/run_on_all_no_pass.sh emerge --sync
 
 sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.83:/layman/  /var/lib/layman/
@@ -17,3 +19,5 @@ sh run_on_all_no_pass.sh emerge -uDvNFp system >> /root/fetch.txt
 sh run_on_all_no_pass.sh emerge -ep world >> /root/fetch.txt
 
 sh /root/shell-scripts/run_on_all_no_pass.sh /root/shell-scripts/glsa-fix.sh
+
+sh /root/shell-scripts/run_on_all_no_pass.sh rsync -ax --delete --progress rsync://192.168.2.83/clamav-db /var/lib/clamav

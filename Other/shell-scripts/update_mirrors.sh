0733ff5a2a9826657d6cccd4c4ab42ae4689c3c0
John M. Drescher <drescherjm@gmail.com>
2015-09-04 14:59:23 -0400

    updated mirrors

--- Other/shell-scripts/update_mirrors.sh
+++ Other/shell-scripts/update_mirrors.sh
@@ -4,4 +4,8 @@ sed -i '/GENTOO_MIRRORS/d' /etc/portage/make.conf
 
 #echo "GENTOO_MIRRORS=\"http://gentoo.mirrors.pair.com/ ftp://gentoo.mirrors.pair.com/ http://mirror.mcs.anl.gov/pub/gentoo/ http://mirror.lug.udel.edu/pub/gentoo/ ftp://mirror.mcs.anl.gov/pub/gentoo/\"" >> /etc/portage/make.conf
 
-GENTOO_MIRRORS="http://gentoo.mirrors.pair.com/ ftp://gentoo.mirrors.pair.com/ http://gentoo.cites.uiuc.edu/pub/gentoo/ http://mirror.datapipe.net/gentoo"
+#GENTOO_MIRRORS="http://gentoo.mirrors.pair.com/ ftp://gentoo.mirrors.pair.com/ http://gentoo.cites.uiuc.edu/pub/gentoo/ http://mirror.datapipe.net/gentoo"
+
+GENTOO_MIRRORS="ftp://gentoo.mirrors.pair.com/ http://gentoo.mirrors.tds.net/gentoo ftp://mirror.leaseweb.com/$
+
+

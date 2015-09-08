e310594597daa3837b5953a7ff1fe5bf8df2da6c
John M. Drescher <drescherjm@gmail.com>
2015-09-08 16:36:36 -0400

    fix display_drive_temp_warning_stats

--- Other/shell-scripts/display_drive_temp_warning_stats.sh
+++ Other/shell-scripts/display_drive_temp_warning_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "WARNINGS:"
-grep "TEMPMON: WARNING" /var/log/messages | grep -v "temperature" | awk '{ print $7,$8, $9 }' | sort | uniq -c
+grep "TEMPMON" /var/log/messages | grep ": WARNING" | grep -v "temperature" | awk '{ print $7,$8, $9 }' | sort | uniq -c
 
 echo "CRITICAL:"
-grep "TEMPMON: CRITICAL" /var/log/messages | grep -v "There are" | awk '{ print $7,$8, $9 }' | sort | uniq -c
+grep "TEMPMON" /var/log/messages | grep  ": CRITICAL" | grep -v "There are" | awk '{ print $7,$8, $9 }' | sort | uniq -c

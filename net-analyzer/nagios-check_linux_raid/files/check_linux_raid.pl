cbbe1079407919b1d96858564d288fc8f1880b06
layman <layman@localhost>
2021-02-08 14:30:57 -0500

    update nagios-check_raid to use lib64 instead of lib

--- net-analyzer/nagios-check_linux_raid/files/check_linux_raid.pl
+++ net-analyzer/nagios-check_linux_raid/files/check_linux_raid.pl
@@ -23,7 +23,7 @@
 #	  WARNING md0 status=[UUU_U], recovery=46.4%, finish=123.0min
 
 use strict;
-use lib "/usr/lib/nagios/plugins";
+use lib "/usr/lib64/nagios/plugins";
 use utils qw(%ERRORS);
 
 # die with an error if we're not on Linux

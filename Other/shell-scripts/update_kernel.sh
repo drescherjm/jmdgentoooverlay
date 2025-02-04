7ae1dd718319b71911a679b6476744317a761276
John M. Drescher <drescherjm@gmail.com>
2025-02-03 23:22:55 -0500

    Update kernel version handling

--- Other/shell-scripts/update_kernel.sh
+++ Other/shell-scripts/update_kernel.sh
@@ -5,13 +5,15 @@ if [ ! -f /usr/src/linux/.config ] ; then
 	zcat /proc/config.gz > /usr/src/linux/.config
 fi
 
+local_version="$(date +%Y%m%d-%H%M)-$(uname -n)"
+
 # Now add the date and machine name to the local version
 sed -i '/CONFIG_LOCALVERSION/d' /usr/src/linux/.config
-echo CONFIG_LOCALVERSION=\"-$(date +%Y%m%d-%H%M)-$(uname -n)\" >> /usr/src/linux/.config
+echo CONFIG_LOCALVERSION=\"-${local_version}\" >> /usr/src/linux/.config
 
 if [ ! -e /etc/zfs/vdev_id.conf ]; then
-  genkernel $@ all --save-config --color --install
+  genkernel $@ all --kernel-append-localversion="-${local_version}" --save-config --color --install
   emerge @module-rebuild
 else
-  genkernel $@ all --save-config --color --install --zfs --bootloader=grub2 --callback="emerge --oneshot @module-rebuild sys-fs/zfs" 
+  genkernel $@ all --save-config --kernel-append-localversion="-${local_version}" --color --install --zfs --bootloader=grub2 --callback="emerge --oneshot @module-rebuild sys-fs/zfs" 
 fi

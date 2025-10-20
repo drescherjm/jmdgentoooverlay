9bbd628675d3732d625dd7545367bd9d4bf37f9d
layman <layman@localhost>
2025-10-19 20:10:49 -0400

    Remove some old ebuilds I am no longer working on.

--- Other/shell-scripts/update_kernel.sh
+++ Other/shell-scripts/update_kernel.sh
@@ -13,8 +13,10 @@ sed -i '/CONFIG_LOCALVERSION/d' /usr/src/linux/.config
 echo CONFIG_LOCALVERSION=\"-${local_version}\" >> /usr/src/linux/.config
 
 if [ ! -e /etc/zfs/vdev_id.conf ]; then
-  genkernel $@ all --kernel-append-localversion="-${local_version}" --save-config --color --install
-  emerge @module-rebuild
+#  genkernel $@ all --kernel-append-localversion="-${local_version}" --save-config --color --install
+ genkernel $@ all  --save-config --color --install
+ emerge @module-rebuild
 else
-  genkernel $@ all --save-config --kernel-append-localversion="-${local_version}" --color --install --zfs --bootloader=grub2 --callback="emerge --oneshot @module-rebuild sys-fs/zfs" 
+#  genkernel $@ all --save-config --kernel-append-localversion="-${local_version}" --color --install --zfs --bootloader=grub2 --callback="emerge --oneshot @module-rebuild sys-fs/zfs" 
+  genkernel $@ all --save-config --color --install --zfs --bootloader=grub2 --callback="emerge --oneshot @module-rebuild sys-fs/zfs" 
 fi
